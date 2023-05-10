1991ad8c590f6102186834edbcaf6caa46dad098
Kelsey Kuno <kelsey.kuno@gmail.com>
2023-05-10 15:08:17 -0500

    added nice color palette module; added color palettes to rendering; added a variety of radius size

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const canvasSketch = require('canvas-sketch');
-const {lerp} = require('canvas-sketch-util/math');
+const { lerp } = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
 
 const settings = {
   dimensions: [2048, 2048],
@@ -10,6 +11,8 @@ const settings = {
 };
 
 const sketch = () => {
+  const palette = random.pick(palettes);
+  console.log(palette);
   const createGrid = () => {
     const points = [];
     const count = 50;
@@ -18,7 +21,11 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? .05 : x / (count - 1);
         const v = count <= 1 ? .05 : y / (count - 1);
-        points.push([u, v]);
+        points.push({
+          color: random.pick(palette),
+          position: [u, v],
+          radius: Math.abs(random.gaussian() * .01)
+        });
       }
     }
     return points;
@@ -26,35 +33,43 @@ const sketch = () => {
   const margin = 100;
   //////////////////////draw out regular grid
   // const points = createGrid();
- 
-  
+
+
   ////////////////math.random removes random points in grid
   // const points = createGrid().filter(()=>Math.random()>0.5);
 
   ///////////////////math.random has limitations, here's the same using the random util, which is more refined
   random.setSeed(512);
-  const points = createGrid().filter(()=>random.value()>0.5);
+  const points = createGrid().filter(() => random.value() > 0.5);
 
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach(data => {
+
+      const {
+        position,
+        radius, 
+        color
+      } = data;
+
+      const [u,v] = position;
       ///////this has the artwork start at the very top and bottom of the screen w/out a margin.
       // const x = u * width;
       // const y = v * height;
 
       //this gives the grid some margin
-      const x = lerp(margin, width-margin, u);
-      const y = lerp(margin, height-margin, v);
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
 
 
 
       //draw circles
       context.beginPath();
-      context.arc(x, y, 15, 0, Math.PI * 2);
-      context.fillStyle = 'black';
+      context.arc(x, y, radius * width, 0, Math.PI * 2);
+      context.fillStyle = color;
       context.lineWidth = 40;
       context.fill();
     });
